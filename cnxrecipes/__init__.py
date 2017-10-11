1dc9cdcd7e94e11b3cd8489056359bd74d77ccb8
Ross Reedstrom <reedstrm@gmail.com>
2017-10-11 15:52:16 -0500

    package manifest.json; python3 compatability

--- cnxrecipes/__init__.py
+++ cnxrecipes/__init__.py
@@ -1,11 +1,12 @@
 import json
-from pkg_resources import resource_stream, resource_string
+from pkg_resources import resource_string
 
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
 
-recipes = json.load(resource_stream(__name__, 'recipes/manifest.json'))
+recipes = json.loads(resource_string(__name__,
+                                     'recipes/manifest.json').decode('utf-8'))
 # Convert filenames to strings
 for recipe in recipes:
     recipe['file'] = resource_string(__name__,
