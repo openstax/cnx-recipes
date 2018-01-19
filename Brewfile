730300214034cca5a10053cd7c1a87e0a3c8e586
Philip Schatz <phil@cnx.org>
2018-01-19 13:40:54 -0600

    fix pyICU compiling
    Original error:
    
    ```
    Building PyICU 2.0.2 for ICU 60.1
        60
    
        Could not configure CXXFLAGS with icu-config
    ...
    ```

--- Brewfile
+++ Brewfile
@@ -3,4 +3,5 @@ brew "python"
 brew "rbenv"
 brew "lcov"
 brew "shellcheck"
-# brew "icu4c" # needed by cnx-easybake. Commented because it is already installed because of node
+brew "icu4c" # needed by cnx-easybake. Commented because it is already installed because of node
+brew "intltool"
--- script/_bootstrap.sh
+++ script/_bootstrap.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# from https://tobywf.com/2017/05/installing-pyicu-on-macos/
-if [[ ! ${ICU_VERSION} ]]; then
-  if [[ "$(uname -s)" = "Darwin" ]]; then
+if [[ "$(uname -s)" = "Darwin" ]]; then
+  # from https://tobywf.com/2017/05/installing-pyicu-on-macos/
+  if [[ ! ${ICU_VERSION} ]]; then
     path_to_icu4c="/usr/local/Cellar/icu4c"
     icu_version=$(ls ${path_to_icu4c})
     icu_version_major="${icu_version%.*}"
@@ -10,5 +10,9 @@ if [[ ! ${ICU_VERSION} ]]; then
     export ICU_VERSION="${icu_version_major}"
     export PYICU_INCLUDES="${path_to_icu4c}/${icu_version}/include"
     export PYICU_LFLAGS="-L${path_to_icu4c}/${icu_version}/lib"
+  fi
+
+  if [[ ! $(which icu-config) ]]; then
+    try brew link icu4c gettext --force
   fi
 fi
