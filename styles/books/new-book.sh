37e16dc3e9cfe11c5d6ef074b2c651e519eee9ec
tomjw64 <tom.jw64@gmail.com>
2018-05-17 09:55:39 -0500

    Fix script for OSX

--- styles/books/new-book.sh
+++ styles/books/new-book.sh
@@ -17,7 +17,9 @@ cd "${0%/*}"
 book_template_name="__book-template__"
 cp -r $book_template_name $1
 cd $1
-rename "s/__book-template__/$1/" ./*
+for file_or_dir in $(find . -name "*__book-template__*"); do
+  mv "$file_or_dir" "${file_or_dir/__book-template__/$1}"
+done
 grep -rl "__book-template__" . | xargs sed -i "s/__book-template__/$1/g"
 grep -rl "__template-theme__" . | xargs sed -i "s/__template-theme__/$2/g"
 cd $pwd
