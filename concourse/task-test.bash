6668128cf873232cb42fed856311abcc44d238b0
Philip Schatz <253202+philschatz@users.noreply.github.com>
2019-02-18 23:53:27 -0600

    try moving Dockerfile instructions into here

--- concourse/task-test.bash
+++ concourse/task-test.bash
@@ -1,3 +1,23 @@
 #!/bin/bash
 
+# Try copying the instructions from Dockerfile into here:
+apt-get update
+apt-get install libxml2-utils xsltproc
+
+# Install pyenv
+curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
+PATH="/root/.pyenv/bin:$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+
+
+# Install nodenv
+git clone https://github.com/nodenv/nodenv.git /root/.nodenv && \
+    git clone https://github.com/nodenv/node-build.git /root/.nodenv/plugins/node-build && \
+    git clone https://github.com/nodenv/nodenv-package-rehash.git /root/.nodenv/plugins/nodenv-package-rehash && \
+    git clone https://github.com/nodenv/nodenv-update.git /root/.nodenv/plugins/nodenv-update
+PATH=/root/.rbenv/shims:/root/.rbenv/bin:/root/.nodenv/shims:/root/.nodenv/bin:$PATH
+
+
+$(pwd)/resource-repo/script/setup
 $(pwd)/resource-repo/script/test
