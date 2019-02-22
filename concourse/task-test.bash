a142ce11616a848e399d2786f34f3bd2a796e791
Philip Schatz <253202+philschatz@users.noreply.github.com>
2019-02-22 13:23:03 -0600

    use the cnx-recipes image

--- concourse/task-test.bash
+++ concourse/task-test.bash
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-# Install OS dependencies
-apt-get update
-apt-get install \
-    libxml2-utils \
-    xsltproc
-
 $(pwd)/resource-repo/script/setup
 $(pwd)/resource-repo/script/test
