45da264e432704b87838f610189e043cc66fc547
Philip Schatz <253202+philschatz@users.noreply.github.com>
2019-02-25 17:24:42 -0600

    add shellcheck exception for linting

--- concourse/task-test.bash
+++ concourse/task-test.bash
@@ -26,5 +26,8 @@ remote_dir="/var/www/repo/cnx-rulesets/$(cat .git/ref)/"
 
 local_styleguide_dir="./styleguide/"
 
+# shellcheck disable=SC2029
 ssh -o "StrictHostKeyChecking no" -o UserKnownHostsFile=/dev/null -i "${key_path}" ${remote_user}@${remote_host} "mkdir -p '${remote_dir}'" || exit 5
+
+# shellcheck disable=SC2029
 rsync -avp -e "ssh -o 'StrictHostKeyChecking no' -o UserKnownHostsFile=/dev/null -i '${key_path} '-l${remote_user}" "${local_styleguide_dir}" "${remote_host}:${remote_dir}" || exit 6
