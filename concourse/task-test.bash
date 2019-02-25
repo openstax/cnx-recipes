7731f33fd0a8b6dec0923fded25a726d7a186ef0
Philip Schatz <253202+philschatz@users.noreply.github.com>
2019-02-25 14:15:28 -0600

    ensure pyenv is initialized

--- concourse/task-test.bash
+++ concourse/task-test.bash
@@ -6,6 +6,9 @@ apt-get install libxml2-utils xsltproc
 # so pyenv uses .python-version (& nodenv)
 cd "$(pwd)"/resource-repo/ || exit 111
 
+# Temp: ensure pyenv is initialized
+eval "$(pyenv init -)"
+
 npm install --global yarn
 
 ./script/setup
