9a3db4a6eaa667b29bece27a2cb14396d91c364c
Philip Schatz <253202+philschatz@users.noreply.github.com>
2019-02-25 16:52:21 -0600

    remove host checking

--- concourse/task-test.bash
+++ concourse/task-test.bash
@@ -26,5 +26,5 @@ remote_dir="/var/www/repo/cnx-rulesets/$(cat .git/ref)/"
 
 local_styleguide_dir="./styleguide/"
 
-ssh -i "${key_path}" ${remote_user}@${remote_host} "mkdir -p '${remote_dir}'" || exit 5
-rsync -avp -e "ssh -i '${key_path} '-l${remote_user}" "${local_styleguide_dir}" "${remote_host}:${remote_dir}" || exit 6
+ssh -o "StrictHostKeyChecking no" -o UserKnownHostsFile=/dev/null -i "${key_path}" ${remote_user}@${remote_host} "mkdir -p '${remote_dir}'" || exit 5
+rsync -avp -e "ssh -o 'StrictHostKeyChecking no' -o UserKnownHostsFile=/dev/null -i '${key_path} '-l${remote_user}" "${local_styleguide_dir}" "${remote_host}:${remote_dir}" || exit 6
