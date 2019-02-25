3e20b8e5b2224a04c6cf509ef1161d30dddfa6ec
Philip Schatz <253202+philschatz@users.noreply.github.com>
2019-02-25 15:54:22 -0600

    do we have enough info to upload?

--- concourse/task-test.bash
+++ concourse/task-test.bash
@@ -3,8 +3,10 @@
 apt-get update
 apt-get install libxml2-utils xsltproc
 
+root_dir=$(pwd)
+
 # so pyenv uses .python-version (& nodenv)
-cd "$(pwd)"/resource-repo/ || exit 111
+cd "${root_dir}"/resource-repo/ || exit 111
 
 # Temp: ensure pyenv is initialized
 eval "$(pyenv init -)"
@@ -13,3 +15,21 @@ npm install --global yarn
 
 ./script/setup
 ./script/test
+
+# Host the built styleguide
+key_path="${root_dir}"/scp-package-secret-key
+echo "${SCP_PACKAGE_SECRET_KEY}" > "${key_path}"
+
+echo "TODO: Sending styleguides for commit $(cat .git/ref)"
+
+# remote_user="rundeck"
+# remote_host="packages.cnx.org"
+# remote_dir="/var/www/repo/cnx-rulesets/$(cat .git/ref)/"
+# remote_master_dir="/var/www/repo/cnx-rulesets/master/"
+
+# local_styleguide_dir="./styleguide/"
+
+# ssh -i "${key_path}" ${remote_user}@${remote_host} "mkdir -p '${remote_dir}'" || exit 5
+# rsync -avp -e "ssh -i '${key_path} '-l${remote_user}" "${local_styleguide_dir}" "${remote_host}:${remote_dir}" || exit 6
+
+# rsync -avp -e "ssh -i ${key_path} -l${remote_user}" ${local_styleguide_dir} ${remote_host}:${remote_master_dir} || exit 6
