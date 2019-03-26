b893d12db5531761a8eb476f8110b4d5180c002e
Philip Schatz <253202+philschatz@users.noreply.github.com>
2019-03-26 13:30:00 -0500

    update type definitions

--- styles/index.d.ts
+++ styles/index.d.ts
@@ -3,5 +3,5 @@ export enum PLATFORM {
   PDF = 'PDF'
 }
 
-export var getStyleFiles: (platform: PLATFORM) => Promise<Map<string, string>>
-export var getStyleContents: (platform: PLATFORM) => Promise<Map<string, string>>
+export var getStyleFiles: (platform: PLATFORM) => Map<string, string>
+export var getStyleContents: (platform: PLATFORM) => Map<string, string>
