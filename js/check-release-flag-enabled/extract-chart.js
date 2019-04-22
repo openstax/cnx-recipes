dd7c00fc839dbf7a9d82a3cdec87cc96df2f8808
Philip Schatz <253202+philschatz@users.noreply.github.com>
2019-04-22 15:27:18 -0500

    lint the JS file

--- js/check-release-flag-enabled/extract-chart.js
+++ js/check-release-flag-enabled/extract-chart.js
@@ -1,31 +1,30 @@
-const getStdin = require('get-stdin');
-const sassExtract = require('sass-extract');
-const order = process.argv[2];
+const getStdin = require('get-stdin')
+const sassExtract = require('sass-extract')
+const order = process.argv[2]
 
 getStdin().then(recipeFeaturesScss => {
   sassExtract.render({
     data: recipeFeaturesScss
   }).then(rendered => {
-    recipeName = rendered.vars['global']['$RECIPE_NAME']['value'];
-    features = rendered.vars['global']['$RELEASE_FLAGS']['value'];
+    const recipeName = rendered.vars['global']['$RECIPE_NAME']['value']
+    const features = rendered.vars['global']['$RELEASE_FLAGS']['value']
 
-    if(order == 0) {
-      header_row = "| Recipe Name |";
-      header_border = "| --- |";
-      columns = Object.keys(features).length
+    if (order === 0) {
+      let headerRow = '| Recipe Name |'
+      let headerBorder = '| --- |'
       Object.keys(features).forEach(featureName => {
-        header_row += ` ${featureName} |`;
-        header_border += " --- |";
-      });
-      header = header_row + "\n" + header_border;
-      console.log(header);
+        headerRow += ` ${featureName} |`
+        headerBorder += ' --- |'
+      })
+      const header = headerRow + '\n' + headerBorder
+      console.log(header)
     }
-    row = `| ${recipeName} |`;
+    let row = `| ${recipeName} |`
     Object.keys(features).forEach(featureName => {
-      enabled = features[featureName]['value']
-      mark = (enabled ? ":heavy_check_mark:" : ":x:")
-      row += ` ${mark} |`;
-    });
-    console.log(row);
-  }).catch(err => { console.error(err) });
-}).catch(err => { console.error(err) });
+      const enabled = features[featureName]['value']
+      const mark = (enabled ? ':heavy_check_mark:' : ':x:')
+      row += ` ${mark} |`
+    })
+    console.log(row)
+  }).catch(err => { console.error(err) })
+}).catch(err => { console.error(err) })
