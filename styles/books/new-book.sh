ba28ae6211cdcf8e6866796b018ca1fa00f6d725
brittany-johnson <brittanyj.email@gmail.com>
2019-04-24 21:54:11 -0500

    shuffling things around

--- styles/books/new-book.sh
+++ styles/books/new-book.sh
@@ -14,23 +14,23 @@ fi
 echo "Building template for $1 under theme $2."
 working_dir="$(pwd)"
 cd "${0%/*}"
-book_template_name="__book-template__"
+book_template_name="intro-business"
 cp -r $book_template_name $1
 cd $1
-for file_or_dir in $(find . -name "*__book-template__*"); do
-  mv "$file_or_dir" "${file_or_dir/__book-template__/$1}"
+for file_or_dir in $(find . -name "*intro-business*"); do
+  mv "$file_or_dir" "${file_or_dir/intro-business/$1}"
 done
-if grep -rl "__book-template__" . | xargs sed -i '' "s/__book-template__/$1/g" >/dev/null 2>&1; then
+if grep -rl "intro-business" . | xargs sed -i '' "s/intro-business/$1/g" >/dev/null 2>&1; then
   echo "OSX sed succeeded."
 else
   echo "OSX sed failed. Trying GNU sed."
-  grep -rl "__book-template__" . | xargs sed -i "s/__book-template__/$1/g"
+  grep -rl "intro-business" . | xargs sed -i "s/intro-business/$1/g"
 fi
-if grep -rl "__template-theme__" . | xargs sed -i '' "s/__template-theme__/$2/g" >/dev/null 2>&1; then
+if grep -rl "theme3" . | xargs sed -i '' "s/theme3/$2/g" >/dev/null 2>&1; then
   echo "OSX sed succeeded."
 else
   echo "OSX sed failed. Trying GNU sed."
-  grep -rl "__template-theme__" . | xargs sed -i "s/__template-theme__/$2/g"
+  grep -rl "theme3" . | xargs sed -i "s/theme3/$2/g"
 fi
 cd $pwd
 echo "Done."
