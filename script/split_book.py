8c808f5f01992e0dd83c9c1cd6dd4d47fc885a03
Philip Schatz <253202+philschatz@users.noreply.github.com>
2020-01-27 14:27:55 -0600

    script to split the book into separate HTML files

---
+++ script/split_book.py
@@ -0,0 +1,23 @@
+import sys
+from pathlib import Path
+from shutil import rmtree
+from cnxepub.collation import reconstitute
+from cnxepub.formatters import DocumentContentFormatter
+from cnxepub.models import flatten_to, Document
+
+
+if len(sys.argv) != 3:
+    raise ValueError('2 arguments are necessary: data/BOOK_NAME/collection.baked.xhtml and the output dir data/BOOK_NAME/disassembled')
+
+with open(sys.argv[1], "rb") as file:
+    binder = reconstitute(file)
+
+out_dir = Path(sys.argv[2]).resolve()
+try:
+    rmtree(out_dir)
+except FileNotFoundError:
+    pass
+out_dir.mkdir()
+for doc in flatten_to(binder, lambda d: isinstance(d, Document)):
+    with open(f"{sys.argv[2]}/{doc.ident_hash}.xhtml", "wb") as out:
+        out.write(bytes(DocumentContentFormatter(doc)))
